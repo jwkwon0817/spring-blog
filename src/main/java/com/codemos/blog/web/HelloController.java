802730b8861ab3204f98186b4c58faa9e856ad0b
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-06-30 00:56:35 +0900

    [ #2 ] find 추가

--- src/main/java/com/codemos/blog/web/HelloController.java
+++ src/main/java/com/codemos/blog/web/HelloController.java
@@ -1,13 +1,14 @@
 package com.codemos.blog.web;
 
+import com.codemos.blog.domain.dto.request.BlogRequestDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
-	@GetMapping("/")
-	public String index() {
-		return "Hello World!";
+	@GetMapping("/test")
+	public BlogRequestDto index() {
+		return new BlogRequestDto("author", "title", "content");
 	}
 }
